1d3a3ff9ae69ee45213e0122776e4b64a33d0cfb
Antoniy Chonkov <antoniy@chonkov.net>
2020-05-25 12:24:19 +0300

    xmonad: add scratchpads and do refactoring

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -29,6 +29,7 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageHelpers (isFullscreen, isDialog,  doFullFloat, doCenterFloat) 
 import XMonad.Hooks.InsertPosition
+import XMonad.Hooks.SetWMName
 
 import XMonad.Util.Run(spawnPipe, hPutStrLn)
 import XMonad.Util.SpawnOnce(spawnOnce)
@@ -64,28 +65,30 @@ myConfig bar0 bar1 = desktopConfig
   , focusedBorderColor = "#bbc5ff"
   , mouseBindings      = myMouseBindings
   , layoutHook         = myLayoutHook
-  , manageHook         = insertPosition End Newer <+> myManageHook <+> manageHook desktopConfig <+> manageDocks <+> namedScratchpadManageHook scratchpads
-  , handleEventHook    = myEventHook <+> fullscreenEventHook
-  , logHook            = dynamicLogWithPP $ namedScratchpadFilterOutWorkspacePP $ myPP bar0 bar1
+  , manageHook         = myManageHook 
+  , handleEventHook    = myEventHook
+  , logHook            = myLogHook bar0 bar1
   , startupHook        = myStartupHook
   } 
 
--- -------- Xmobar pretty prints {{{1
+-- -------- Log Hook and PP {{{1
 
 windowCount = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset
 
-myPP bar0 bar1 = xmobarPP 
-  { ppOutput          = \x -> hPutStrLn bar0 x  >> hPutStrLn bar1 x
-  , ppCurrent         = xmobarColor "#c3e88d" "" . wrap "[" "]" -- Current workspace in xmobar
-  , ppVisible         = xmobarColor "#c3e88d" ""                -- Visible but not current workspace
-  , ppHidden          = xmobarColor "#82AAFF" "" . wrap "*" ""  -- Hidden workspaces in xmobar
-  , ppHiddenNoWindows = xmobarColor "#F07178" ""                -- Hidden workspaces (no windows)
-  , ppTitle           = xmobarColor "#d0d0d0" "" . shorten 80   -- Title of active window in xmobar
-  , ppSep             = "<fc=#666666> | </fc>"                  -- Separators in xmobar
-  , ppUrgent          = xmobarColor "#C45500" "" . wrap "!" "!" -- Urgent workspace
-  , ppExtras          = [windowCount]                           -- # of windows current workspace
-  , ppOrder           = \(ws:l:t:ex) -> [ws,l]++ex++[t]
-  }
+myLogHook bar0 bar1 = dynamicLogWithPP 
+  $ namedScratchpadFilterOutWorkspacePP 
+  $ xmobarPP 
+    { ppOutput          = (\x -> hPutStrLn bar0 x >> hPutStrLn bar1 x)
+    , ppCurrent         = xmobarColor "#c3e88d" "" . wrap "[" "]" -- Current workspace in xmobar
+    , ppVisible         = xmobarColor "#c3e88d" ""                -- Visible but not current workspace
+    , ppHidden          = xmobarColor "#82AAFF" "" . wrap "*" ""  -- Hidden workspaces in xmobar
+    , ppHiddenNoWindows = xmobarColor "#F07178" ""                -- Hidden workspaces (no windows)
+    , ppTitle           = xmobarColor "#d0d0d0" "" . shorten 80   -- Title of active window in xmobar
+    , ppSep             = "<fc=#666666> | </fc>"                  -- Separators in xmobar
+    , ppUrgent          = xmobarColor "#C45500" "" . wrap "!" "!" -- Urgent workspace
+    , ppExtras          = [windowCount]                           -- # of windows current workspace
+    , ppOrder           = \(ws:l:t:ex) -> [ws,l]++ex++[t]
+    }
 
 -- -------- Keybindings {{{1
 
@@ -112,12 +115,12 @@ myKeys =
   , ("M-m"          , windows W.focusMaster)       -- Move focus to the master window
   , ("M-j"          , windows W.focusDown)         -- Move focus to the next window
   , ("M-k"          , windows W.focusUp)           -- Move focus to the prev window
+  , ("M1-<Tab>"     , windows W.focusDown)         -- Move focus to the next window
+  , ("M1-S-<Tab>"   , windows W.focusUp)           -- Move focus to the prev window
   -- , ("M-S-m"        , windows W.swapMaster)        -- Swap the focused window and the master window
   , ("M-S-j"        , windows W.swapDown)          -- Swap the focused window with the next window
   , ("M-S-k"        , windows W.swapUp)            -- Swap the focused window with the prev window
   , ("M-<Backspace>", promote)                     -- Moves focused window to master, all others maintain order
-  , ("M1-<Tab>"     , moveTo Next nonEmptyNonNSP)  -- (Alt-Shift-Tab) Rotate all windows except master and keep focus in place
-  , ("M1-S-<Tab>"   , moveTo Prev nonEmptyNonNSP)  -- (Alt-Shift-Tab) Rotate all windows except master and keep focus in place
   , ("M-<Page_Up>"  , rotSlavesUp)                 -- (Alt-Shift-Tab) Rotate all windows except master and keep focus in place
   , ("M-<Page_Down>", rotSlavesDown)               -- (Alt-Shift-Tab) Rotate all windows except master and keep focus in place
   , ("M-<Home>"     , rotAllUp)                    -- (Alt-Ctrl-Tab) Rotate all the windows in the current stack
@@ -149,8 +152,10 @@ myKeys =
   , ("M-S-,", shiftPrevScreen) -- Shifts focused window to previous screen
 
   -- Scratchpads
-  , ("<F12>"  , namedScratchpadAction scratchpads "scratchpad_tmux")
-  , ("M-<F12>", namedScratchpadAction scratchpads "scratchpad_code")
+  , ("<F12>"  , namedScratchpadAction scratchpads "tmux")
+  , ("M-<F12>", namedScratchpadAction scratchpads "note")
+  , ("M-<F10>", namedScratchpadAction scratchpads "htop")
+  , ("M-<F9>" , namedScratchpadAction scratchpads "pavucontrol")
   ] where nonNSP          = WSIs (return (\ws -> W.tag ws /= "nsp"))
           nonEmptyNonNSP  = WSIs (return (\ws -> isJust (W.stack ws) && W.tag ws /= "nsp"))
           zoom            = sendMessage (Toggle NBFULL) >> sendMessage ToggleStruts
@@ -172,31 +177,39 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- specify per-workspace layouts
 wsLayouts idx layouts = onWorkspace (myWorkspaces !! idx) layouts
 
-myLayoutHook = avoidStruts $ smartBorders $ toggleLayouts floats $ mkToggle (NBFULL ?? NOBORDERS ?? EOT) $ 
-  mkToggle (single REFLECTX) $ mkToggle (single REFLECTY) $
-  wsLayouts 0 wwwLayouts $
-  wsLayouts 1 chatLayouts $
-  wsLayouts 2 devLayouts $
-  myDefaultLayout 
+myLayoutHook = avoidStruts 
+  $ smartBorders 
+  $ toggleLayouts floats 
+  $ fullScreenToggle
+  $ reflectXToggle
+  $ reflectYToggle
+  $ wsLayouts 0 wwwLayouts
+  $ wsLayouts 1 chatLayouts
+  $ wsLayouts 2 devLayouts
+  $ myDefaultLayout 
   where 
+    fullScreenToggle = mkToggle (NBFULL ?? NOBORDERS ?? EOT) 
+    reflectXToggle = mkToggle (single REFLECTX) 
+    reflectYToggle = mkToggle (single REFLECTY) 
+
     wwwLayouts = monocle ||| tall ||| tab
     chatLayouts = oneBig ||| grid ||| monocle ||| tab
     devLayouts = monocle ||| tall
     myDefaultLayout = tall ||| oneBig ||| grid ||| monocle ||| tab ||| floats
 
-mySpacing = spacingRaw 
-  True             -- smartBorders
-  (Border 0 3 3 3) -- screen borders
-  True             -- screen borders enabled
-  (Border 5 5 5 5) -- window borders
-  True             -- window borders enabled
+    mySpacing = spacingRaw 
+      True             -- smartBorders
+      (Border 0 3 3 3) -- screen borders
+      True             -- screen borders enabled
+      (Border 3 3 3 3) -- window borders
+      True             -- window borders enabled
 
-tall    = renamed [Replace "tall"]    $ limitWindows 6  $ mySpacing $ mkToggle (single MIRROR) $ ResizableTall 1 (3/100) (1/2) []
-grid    = renamed [Replace "grid"]    $ limitWindows 12 $ mySpacing $ mkToggle (single MIRROR) $ Grid (16/10)
-oneBig  = renamed [Replace "oneBig"]  $ limitWindows 6  $ mySpacing $ mkToggle (single MIRROR) $ OneBig (5/9) (8/12)
-monocle = renamed [Replace "monocle"] $ limitWindows 20 $ noBorders $ Full
-tab     = renamed [Replace "tabbed"]  $ limitWindows 20 $ noBorders $ tabbed shrinkText (theme adwaitaDarkTheme)
-floats  = renamed [Replace "floats"]  $ limitWindows 20 $ simplestFloat
+    tall    = renamed [Replace "tall"]    $ limitWindows 6  $ mySpacing $ mkToggle (single MIRROR) $ ResizableTall 1 (3/100) (1/2) []
+    grid    = renamed [Replace "grid"]    $ limitWindows 12 $ mySpacing $ mkToggle (single MIRROR) $ Grid (16/10)
+    oneBig  = renamed [Replace "oneBig"]  $ limitWindows 6  $ mySpacing $ mkToggle (single MIRROR) $ OneBig (5/9) (8/12)
+    monocle = renamed [Replace "monocle"] $ limitWindows 20 $ noBorders $ Full
+    tab     = renamed [Replace "tabbed"]  $ limitWindows 20 $ noBorders $ tabbed shrinkText (theme adwaitaDarkTheme)
+    floats  = renamed [Replace "floats"]  $ limitWindows 20 $ simplestFloat
 
 -- -------- Workspaces {{{1
 
@@ -206,7 +219,7 @@ xmobarEscape = concatMap el
     el x   = [x]
         
 myWorkspaces :: [String]   
-myWorkspaces = clickable . (map xmobarEscape) $ ["www","chat","dev","editor","game","media","audio","sys","aux"]
+myWorkspaces = clickable . (map xmobarEscape) $ ["www","chat","dev","game","media","aux"]
   where 
     clickable l = [ "<action=xdotool key super+" ++ show (n) ++ ">" ++ ws ++ "</action>" |
                     (i, ws) <- zip [1..9] l,                                        
@@ -220,52 +233,62 @@ matchApp c = className =? c <||> title =? c <||> resource =? c
 -- move window to workspace (idx)
 moveToWs idx = doShift (myWorkspaces !! idx)
 
-myManageHook ::  ManageHook
-myManageHook = composeAll . concat $
-    [ [ isDialog     --> doCenterFloat               ]
-    , [ isFullscreen --> doFullFloat                 ]
-    , [ matchApp c   --> doIgnore      | c <- bars   ]
-    , [ matchApp c   --> doFloat       | c <- float  ]
-    , [ matchApp c   --> doCenterFloat | c <- cfloat ]
-    , [ matchApp c   --> moveToWs 0    | c <- www    ]
-    , [ matchApp c   --> moveToWs 1    | c <- chat   ]
-    , [ matchApp c   --> moveToWs 2    | c <- dev    ]
-    , [ matchApp c   --> moveToWs 3    | c <- editor ]
-    , [ matchApp c   --> moveToWs 4    | c <- game   ]
-    , [ matchApp c   --> moveToWs 5    | c <- media  ]
-    , [ matchApp c   --> moveToWs 6    | c <- audio  ]
-    , [ role =?  "browser"   --> moveToWs 0    | c <- www    ]
-    ]
-    where    
-        bars   = ["xmobar", "trayer"]
-        float  = ["nm-connection-editor", "jetbrains-toolbox"]
-        cfloat = ["mpv", "Gimp-2.10"]
-        www    = ["Chromium", "Brave-browser", "Mozilla Firefox"]
-        chat   = ["ViberPC", "TelegramDesktop", "Slack", "discord", "Skype", "Keybase"]
-        dev    = ["jetbrains-idea", "JetBrains Toolbox"]
-        game   = ["Steam"]
-        editor = ["libreoffice-calc", "libreoffice-writer", "VirtualBox", "libreoffice", "spacefm", "zathura", "code-oss", "Subl3"]
-        media  = ["mpv", "vlc", "plexmediaplayer"]
-        audio  = ["Audacity", "Pavucontrol"]
-        role   = stringProperty "WM_WINDOW_ROLE"
+-- myManageHook ::  ManageHook
+myManageHook = insertPosition End Newer 
+  <+> manageHook desktopConfig 
+  <+> manageDocks 
+  <+> namedScratchpadManageHook scratchpads
+  <+> appsManageHook
 
-myEventHook = mempty
+appsManageHook = composeAll . concat $
+  [ [ isDialog     --> doCenterFloat               ]
+  , [ isFullscreen --> doFullFloat                 ]
+  , [ matchApp c   --> doIgnore      | c <- bars   ]
+  , [ matchApp c   --> doFloat       | c <- float  ]
+  , [ matchApp c   --> doCenterFloat | c <- cfloat ]
+  , [ matchApp c   --> moveToWs 0    | c <- www    ]
+  , [ matchApp c   --> moveToWs 1    | c <- chat   ]
+  , [ matchApp c   --> moveToWs 2    | c <- dev    ]
+  , [ matchApp c   --> moveToWs 3    | c <- game   ]
+  , [ matchApp c   --> moveToWs 4    | c <- media  ]
+  , [ role =?  "browser"   --> moveToWs 0    | c <- www    ]
+  ]
+  where    
+      bars   = ["xmobar", "trayer"]
+      float  = ["nm-connection-editor", "jetbrains-toolbox", "Xmessage"]
+      cfloat = ["mpv", "Gimp-2.10"]
+      www    = ["Chromium", "Brave-browser", "Mozilla Firefox"]
+      chat   = ["ViberPC", "TelegramDesktop", "Slack", "discord", "Skype", "Keybase"]
+      dev    = ["jetbrains-idea", "JetBrains Toolbox"]
+      game   = ["Steam"]
+      media  = ["mpv", "vlc", "plexmediaplayer", "Audacity"]
+      role   = stringProperty "WM_WINDOW_ROLE"
+
+-- -------- Event Hook {{{1
+
+-- myEventHook = mempty
+myEventHook = fullscreenEventHook
 
 -- -------- Startup {{{1
 
 myStartupHook = do
+  setWMName "LG3D"
   spawnOnce "trayer --edge top --align right --widthtype request --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x282A36 --height 20 --monitor 0 --iconspacing 2 --padding 5 &"
   spawnOnce "brave &"
   spawnOnce "telegram-desktop &"
   spawnOnce "viber &"
   spawnOnce "slack &"
   spawnOnce "discord &"
-  spawnOnce "pavucontrol &"
   
 -- -------- Scratchpads {{{1
 
-scratchpads = [ NS "scratchpad_tmux" spawnTerm findTerm manageTerm 
-              , NS "scratchpad_code" spawnCode findCode manageCode 
+nsFloat l t w h = customFloating $ W.RationalRect l t w h
+
+scratchpads = [ NS "tmux"        spawnTerm findTerm manageTerm 
+              , NS "note"        spawnNote findNote manageNote 
+              , NS "pavucontrol" spawnVol  findVol  manageVol
+              , NS "htop"        spawnTop  findTop  manageTop
+              -- , NS "pavucontrol" "pavucontrol" (className =? "Pavucontrol") nsFloat 
               ] 
   where
     spawnTerm  = myTerm ++ " -t scratchpad_tmux -e 'tmuxdd'"
@@ -277,9 +300,27 @@ scratchpads = [ NS "scratchpad_tmux" spawnTerm findTerm manageTerm
         t = 0.95 -h
         l = 0.95 -w
 
-    spawnCode  = "code-oss"
-    findCode   = className =? "code-oss"
-    manageCode = customFloating $ W.RationalRect l t w h 
+    spawnNote  = "code-oss"
+    findNote   = className =? "code-oss"
+    manageNote = customFloating $ W.RationalRect l t w h 
+      where
+        h = 0.9
+        w = 0.9
+        t = 0.95 -h
+        l = 0.95 -w
+
+    spawnVol  = "pavucontrol"
+    findVol   = className =? "Pavucontrol"
+    manageVol = customFloating $ W.RationalRect l t w h 
+      where
+        h = 0.7
+        w = 0.5
+        t = (0.95 -h) / 2
+        l = (0.95 -w) / 2
+
+    spawnTop  = myTerm ++ " -t scratchpad_htop -e htop"
+    findTop   = title =? "scratchpad_htop"
+    manageTop = customFloating $ W.RationalRect l t w h 
       where
         h = 0.9
         w = 0.9
